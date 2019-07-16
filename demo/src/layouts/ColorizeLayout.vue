0849e19e0f945a26946f47594358da14d6e4711d
Jeff Galbraith <jeff.galbraith@xendra.com>
2019-07-16 09:25:01 -0600

    chore(docs): update demo

--- demo/src/layouts/ColorizeLayout.vue
+++ demo/src/layouts/ColorizeLayout.vue
@@ -14,7 +14,7 @@
           <q-icon name="menu"></q-icon>
         </q-btn>
 
-        <q-icon name="far fa-calendar-alt" class="q-ml-md" size="1.5rem"></q-icon>
+        <q-icon name="bolt" class="q-ml-md" size="1.5rem"></q-icon>
 
         <q-toolbar-title v-if="$q.screen.width > 500">
           Colorize
@@ -33,17 +33,7 @@
       content-class="white"
       :width="350"
     >
-      <div class="row justify-around col-12">
-        <q-date
-          v-model="selectedDateForQDate"
-          minimal
-          class="fit"
-          :first-day-of-week="firstDayMonday === true ? 1 : 0"
-          :options="options"
-        ></q-date>
-      </div>
-      <div class="col-12">
-
+      <div>
         <q-expansion-item
           expand-separator
           default-opened
